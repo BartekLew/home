c0b0c40d9207b556812b134dc7c753c1bb459ac8
Lew <barpas@zoho.com>
2019-06-16 16:13:55 +0200

    refactor: separate string-iterator related stuff

--- doc.lisp
+++ doc.lisp
@@ -1,5 +1,6 @@
 (load "html.lisp")
 (load "util.lisp")
+(load "string.lisp")
 
 
 (defclass chunk ()
@@ -119,82 +120,6 @@
 (defmethod chunks= ((cs1 chunk) (cs2 chunk))
 	(and (chunk= cs1 cs2) (chunks= (tail cs1) (tail cs2))))
 
-
-(defclass string-iterator ()
-	((text :initarg :< :accessor text)
-	(pos :initform -1 :accessor pos)))
-
-(defmethod value ((i string-iterator))
-	(text i))
-
-(defmethod print-object ((si string-iterator) out)
-	(format out "<~A @~A '~A'>" (type-of si) (pos si) (s/- (format nil "~A" (value si)) 20)))
-
-(defgeneric next (iter))
-(defmethod next ((i string-iterator))
-	(with-slots (text pos) i
-	(setf pos (+ 1 pos))
-	(if (>= pos (length text)) nil
-		(char text pos))))
-
-(defclass spechar ()
-	((chr :initarg := :reader chr)
-	(action :initarg :! :reader action)))
-
-(defmethod print-object ((sc spechar) out)
-	(format out "<~A:'~A'>" (type-of sc) (chr sc)))
-
-(defmacro replace-fun (new-val)
-	`(lambda (iterator)
-		(with-slots (text pos) iterator
-		(setf text (s+ (subseq text 0 pos)
-				,new-val
-				(subseq text (+ pos 1))))
-		(setf pos (+ pos (length ,new-val)))
-		iterator)))
-
-(defvar *default-spechars* `(
-	,(!+ 'spechar := #\< :! (replace-fun "&lt;"))
-	,(!+ 'spechar := #\> :! (replace-fun "&gt;"))))
-
-(defclass split-string-iterator (string-iterator)
-	((split-part :initarg :<+ :initform '() :reader split-part)))
-
-(defmethod value ((i split-string-iterator))
-	(with-slots (text split-part) i
-	(append split-part (list text))))
-
-(defgeneric split-text (iterator pos))
-(defmethod split-text ((i string-iterator) pos)
-	(with-slots (text) i
-	(!+ 'split-string-iterator :<+ (list (subseq text 0 pos))
-			:< (subseq text (+ pos 1)))))
-
-(defmethod split-text ((i split-string-iterator) pos)
-	(with-slots (split-part text) i
-	(let ((x (call-next-method)))
-	(!+ 'split-string-iterator :<+ (append split-part (split-part x))
-		:< (slot-value x 'text)))))
-
-(defgeneric push-back (collection element))
-(defmethod push-back ((i split-string-iterator) val)
-	(with-slots (split-part) i
-	(setf split-part (append split-part (list val)))
-	i));
-
-(defgeneric discard-text (text-container startpos endpos))
-(defmethod discard-text ((i string-iterator) (startpos (eql 0)) endpos)
-	(with-slots (text) i
-	(setf text (subseq text endpos)) i))
-
-(defmacro region-tag-fun (lim make-tag)
-	`(lambda (iterator)
-		(with-slots (text pos) iterator
-		(let* ((endpos (position ,lim text :start (+ 1 pos))))
-		(discard-text (push-back (split-text iterator pos)
-					(,make-tag (subseq text (+ 1 pos) endpos)))
-				0 (- endpos pos))))))
-
 (defvar *paragraph-spechars* (append *default-spechars* `(
 	,(!+ 'spechar := #\\ :! (lambda (iter)
 		(with-slots (pos) iter
@@ -205,20 +130,6 @@
 	,(!+ 'spechar := #\% :! (region-tag-fun #\%
 		(lambda (content)
 			(!+ 'tag := "i" :< content)))))))
-
-(defgeneric ~format (input &optional spechars))
-
-(defmethod ~format ((input string) &optional (spechars *default-spechars*))
-	(~format (!+ 'string-iterator :< input) spechars))
-
-(defmethod ~format ((input string-iterator) &optional (spechars *default-spechars*))
-	(let ((n (next input)))
-	(if n (let ((spechar (find n spechars
-				:test (lambda (chr spechar)
-					(eql chr (chr spechar))))))
-		(if spechar (~format (apply (action spechar) `(,input)) spechars)
-			(~format input spechars)))
-		(value input))))
 
 (defgeneric >tags (chunk))
 (defmethod >tags ((c chunk))
@@ -232,6 +143,14 @@
 
 (defmethod >tags ((c code-block))
 	(cons (!+ 'tag := "pre" :< (!+ 'tag := "code" :< (~format (value c)))) (call-next-method)))
+
+(defmethod >tags ((k keyval-line))
+	(if (string= (first (value k)) "AUDIO")
+		(cons (!+ 'tag := "audio" :&- "controls"
+			:< (!+ 'tag := "source" :& `("src" ,(second (value k))
+						"type" "audio/mpeg"))) 
+			(call-next-method))
+		(call-next-method)))
 
 
 ;; document class
---
+++ string.lisp
@@ -0,0 +1,88 @@
+(defclass string-iterator ()
+	((text :initarg :< :accessor text)
+	(pos :initform -1 :accessor pos)))
+
+(defmethod value ((i string-iterator))
+	(text i))
+
+(defmethod print-object ((si string-iterator) out)
+	(format out "<~A @~A '~A'>" (type-of si) (pos si) (s/- (format nil "~A" (value si)) 20)))
+
+(defgeneric next (iter))
+(defmethod next ((i string-iterator))
+	(with-slots (text pos) i
+	(setf pos (+ 1 pos))
+	(if (>= pos (length text)) nil
+		(char text pos))))
+
+(defclass spechar ()
+	((chr :initarg := :reader chr)
+	(action :initarg :! :reader action)))
+
+(defmethod print-object ((sc spechar) out)
+	(format out "<~A:'~A'>" (type-of sc) (chr sc)))
+
+(defmacro replace-fun (new-val)
+	`(lambda (iterator)
+		(with-slots (text pos) iterator
+		(setf text (s+ (subseq text 0 pos)
+				,new-val
+				(subseq text (+ pos 1))))
+		(setf pos (+ pos (length ,new-val)))
+		iterator)))
+
+(defvar *default-spechars* `(
+	,(!+ 'spechar := #\< :! (replace-fun "&lt;"))
+	,(!+ 'spechar := #\> :! (replace-fun "&gt;"))))
+
+(defclass split-string-iterator (string-iterator)
+	((split-part :initarg :<+ :initform '() :reader split-part)))
+
+(defmethod value ((i split-string-iterator))
+	(with-slots (text split-part) i
+	(append split-part (list text))))
+
+(defgeneric split-text (iterator pos))
+(defmethod split-text ((i string-iterator) pos)
+	(with-slots (text) i
+	(!+ 'split-string-iterator :<+ (list (subseq text 0 pos))
+			:< (subseq text (+ pos 1)))))
+
+(defmethod split-text ((i split-string-iterator) pos)
+	(with-slots (split-part text) i
+	(let ((x (call-next-method)))
+	(!+ 'split-string-iterator :<+ (append split-part (split-part x))
+		:< (slot-value x 'text)))))
+
+(defgeneric push-back (collection element))
+(defmethod push-back ((i split-string-iterator) val)
+	(with-slots (split-part) i
+	(setf split-part (append split-part (list val)))
+	i));
+
+(defgeneric discard-text (text-container startpos endpos))
+(defmethod discard-text ((i string-iterator) (startpos (eql 0)) endpos)
+	(with-slots (text) i
+	(setf text (subseq text endpos)) i))
+
+(defmacro region-tag-fun (lim make-tag)
+	`(lambda (iterator)
+		(with-slots (text pos) iterator
+		(let* ((endpos (position ,lim text :start (+ 1 pos))))
+		(discard-text (push-back (split-text iterator pos)
+					(,make-tag (subseq text (+ 1 pos) endpos)))
+				0 (- endpos pos))))))
+
+(defgeneric ~format (input &optional spechars))
+
+(defmethod ~format ((input string) &optional (spechars *default-spechars*))
+	(~format (!+ 'string-iterator :< input) spechars))
+
+(defmethod ~format ((input string-iterator) &optional (spechars *default-spechars*))
+	(let ((n (next input)))
+	(if n (let ((spechar (find n spechars
+				:test (lambda (chr spechar)
+					(eql chr (chr spechar))))))
+		(if spechar (~format (apply (action spechar) `(,input)) spechars)
+			(~format input spechars)))
+		(value input))))
